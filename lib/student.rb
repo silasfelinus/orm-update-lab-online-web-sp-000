f61fb31f9d3e9021f85ada2ac9c835bfccb2473a
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-08 01:55:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = self.new(row[0], row[1], row[2])
+    new_student = self.new(row[1], row[2], row[0])
     new_student  # return the newly created instance
   end
 
